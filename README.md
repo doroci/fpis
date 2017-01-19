43bf0d1ac8014bf317e8e10326f54132ccc86ca1
doroci <hiiln0273@gmail.com>
2017-01-19 16:07:05 +0900

    add ch2

---
+++ README.md
@@ -0,0 +1,13 @@
+# Functional Programming in Scala
+
+
+## Reference
+```
+    [스칼라로 배우는 함수형 프로그래밍]
+    http://www.kyobobook.co.kr/product/detailViewKor.laf?ejkGb=KOR&mallGb=KOR&barcode=9791185890180&orderClick=LEA&Kc=
+
+    [scala-exercises]
+    https://www.scala-exercises.org/fp_in_scala/getting_started_with_functional_programming
+```
+
+
--- src/main/scala-2.12/ch2/Ch2.scala
+++ src/main/scala-2.12/ch2/Ch2.scala
@@ -3,7 +3,7 @@ package ch2
 /**
   * Created by lee on 2017. 1. 12..
   */
-object Ch2 {
+object Ch2 extends App{
 
   //팩토리얼
   def factorial(n : Int) : Int = {
@@ -23,10 +23,44 @@ object Ch2 {
     go(n,0,1)
   }
 
-  def main(args: Array[String]) {
-
-    println(factorial(6))
-    println(fib(6))
-
+  //배열에서 문자열을 찾는 단항적 함수 (monomorphic function)
+  def findFirst(ss: Array[String], key: String ) : Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int = {
+      if (n >= ss.length) -100
+      else if (ss(n) == key) n
+      else loop(n+1)
+    }
+    loop(0)
   }
+
+  //배열에서 한 요소를 찾는 다형적 함수
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int = {
+      if(n >= as.length) -100
+      else if (p(as(n))) n
+      else loop(n+1)
+    }
+    loop(0)
+  }
+
+  //Array[A]가 주어진 비교 함수에 의거해서 정렬되어 있는지 점검하는 isSorted 함수를 구현
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean ): Boolean = {
+
+    def go(n: Int): Boolean = {
+      if(n >= as.length-1) true
+      else if(ordered(as(n), as(n+1))) false
+      else go(n+1)
+    }
+    go(0)
+  }
+
+
+    println(s" factorial(6), ${factorial(6)}")
+    println(s" fib(6), ${fib(6)}")
+    println(s" findFirst(Array('a','b','c','d', 'c'), ${findFirst(Array("'a'","'b'","'c'","'d'") ,"'c'")}")
+    println(s" isSorted, ${isSorted(ordered = (x: Int, y:Int) => x > y , as = Array(1,2,3))}")
+    println(s" isSorted, ${isSorted(ordered = (x: Int, y:Int) => x < y , as = Array(1,3,2))}")
 }
+
